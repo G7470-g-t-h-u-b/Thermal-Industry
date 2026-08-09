d9f0a1955eca046882fce069c0d7bf8efbbcfe11
HK7470 <ASUS@LAPTOP-VIEOFEFH>
2026-08-09 19:12:39 +0800

    0.3.0a-alpha34 科技树测试2

--- src/example/content/ModTeachTree.java
+++ src/example/content/ModTeachTree.java
@@ -1,10 +1,13 @@
 package example.content;
 
 
+import mindustry.content.Blocks;
 import mindustry.content.Items;
 import mindustry.content.Planets;
 import mindustry.content.TechTree;
+import mindustry.ctype.UnlockableContent;
 import mindustry.type.Item;
+import mindustry.world.Block;
 
 import static mindustry.content.TechTree.node;
 import static mindustry.content.TechTree.nodeProduce;
@@ -20,15 +23,29 @@ public class ModTeachTree {
         _children.parent=_parent;
         _parent.children.add(_children);
     }
-    public static void load(){
+    public static void addToTechNode(UnlockableContent parent_, UnlockableContent children_){
         TechTree.TechNode[] techNodes=new TechTree.TechNode[]{null};
-        Planets.serpulo.techTree.each(node -> {
-            if(node.content == Items.titanium) techNodes[0] = node;
+        Planets.serpulo.techTree.each(node->{
+            if(node.content==parent_) techNodes[0]=node;
         });
-        TechTree.TechNode titanium_ = techNodes[0];
+        TechTree.TechNode _parent=techNodes[0];
+        TechTree.TechNode _children=nodeProduce(children_,()->{});
+        _children.parent=_parent;
+        _parent.children.add(_children);
+    }
+    public static void load(){
+//        TechTree.TechNode[] techNodes=new TechTree.TechNode[]{null};
+//        Planets.serpulo.techTree.each(node -> {
+//            if(node.content == Items.titanium) techNodes[0] = node;
+//        });
+//        TechTree.TechNode titanium_ = techNodes[0];
+//
+//        TechTree.TechNode fe=nodeProduce(ModItems.ferrum,()->{});
+//        fe.parent=titanium_;
+//        titanium_.children.add(fe);
 
-        TechTree.TechNode fe=nodeProduce(ModItems.ferrum,()->{});
-        fe.parent=titanium_;
-        titanium_.children.add(fe);
+        addToTechNode(Items.titanium,ModItems.hematite);
+        addToTechNode(ModItems.hematite,ModItems.ferrum);
+        addToTechNode(Blocks.siliconSmelter,ModBlocks.scrapSiliconSmelter);
     }
 }
