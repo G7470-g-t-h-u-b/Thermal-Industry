2ba32614f92db21083aa31cd60b702ba6768d4a7
HK7470 <ASUS@LAPTOP-VIEOFEFH>
2026-08-12 20:47:34 +0800

    0.3.0a-alpha39 ?!墙墙!?

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -209,8 +209,6 @@ public class ExampleJavaMod extends Mod{
         }};//汽油
 
 
-        ModBlocks.loadCopperWalls();
-
         ModBlocks.explosive=new RemoveWall("explosive"){{
             requirements(Category.effect,with(Items.lead,50,Items.pyratite,30,Items.blastCompound,10));
         }};
@@ -218,6 +216,7 @@ public class ExampleJavaMod extends Mod{
         ModBlocks.load2();
         ModBlocks.loadFloor();
 
+        ModBlocks.loadCopperWalls();
         ModBlocks.loadWall();
 
         ModBlocks.load3();
